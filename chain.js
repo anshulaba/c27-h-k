2a1d330a40f352984ce8788cae60470dc5e38247
anshulaba <72729339+anshulaba@users.noreply.github.com>
2021-04-18 18:55:17 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,24 @@
+class chain {
+    constructor(bodyA,bodyB)
+    {
+        var options = {
+            length :10,
+            stiffness : 0.4,
+            bodyA : bodyA,
+            bodyB :bodyB
+
+}
+
+this.chain = Constraint.create(options);
+World.add(world,this.chain);
+
+    }
+    display(){
+
+        var pointA= this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+    }
+}
